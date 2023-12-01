f26019375a5be8f3c74207dfccffbcfb291520c1
Zlurgg <joseph.brightman@gmail.com>
2023-12-01 11:37:58 +0000

    dagger hilt implemented in prep for vm and fixed build version issues

--- MainActivity.kt
+++ MainActivity.kt
@@ -4,12 +4,13 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import uk.co.zlurgg.thedaytoworkout.feature_workout_reminder.TheDayToWorkoutApp
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             TheDayToWorkoutApp()
         }
@@ -17,6 +18,5 @@ class MainActivity : ComponentActivity() {
     companion object {
         lateinit  var appContext: Context
     }
-
 }
 
---
+++ TheDayToWorkoutApp.kt
@@ -0,0 +1,7 @@
+package uk.co.zlurgg.thedaytoworkout
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TheDayToWorkoutApp: Application()
